8194f5fbb171afa393a09b63e435ac09261479df
vund2004 <vund261204@gmail.com>
2024-09-26 16:19:28 +0700

    dv_task1007

--- src/pages/Product/ProductList/index.jsx
+++ src/pages/Product/ProductList/index.jsx
@@ -29,7 +29,30 @@ export default function ProductTable() {
         console.error('Dữ liệu sản phẩm không hợp lệ');
         return [];
     }, [showDeleted, data]);
+    // const columns = useMemo(() => [
+    //     { field: 'productId', headerName: 'Mã Sản phẩm', width: 150 },
 
+    //     { field: 'productName', headerName: 'Tên sản phẩm', width: 150 },
+    //     { field: 'category', headerName: 'Loại', width: 120 },
+    //     { field: 'unit', headerName: 'Đơn vị tính', width: 100 },
+    //     { field: 'inventoryCount', headerName: 'Tồn kho', width: 120 },
+    //     { field: 'cost', headerName: 'Giá gốc', width: 120 },
+    //     { field: 'price', headerName: 'Giá bán', width: 120 },
+    //     { field: 'expiryDate', headerName: 'Ngày hết hạn', width: 150 },
+    //     {
+    //         field: 'actions',
+    //         headerName: 'Công cụ',
+    //         width: 150,
+    //         align: 'center',
+    //         renderCell: (params) => (
+    //             <Box display="flex" justifyContent="left" alignItems="center">
+    //                 <IconButton color="primary" onClick={() => handleEdit(params.row)}>
+    //                     <EditIcon />
+    //                 </IconButton>
+    //             </Box>
+    //         ),
+    //     },
+    // ], [showDeleted]);
     const columns = useMemo(() => [
         { field: 'productId', headerName: 'Mã Sản phẩm', width: 150 },
         {
@@ -37,7 +60,11 @@ export default function ProductTable() {
             headerName: 'Hình',
             width: 150,
             renderCell: (params) => (
-                <img src={params.value} alt={params.row.img} style={{ width: '100%', height: 'auto' }} />
+                <img
+                    src={params.value}
+                    alt={params.row.productName}
+                    style={{ width: '100%', height: 'auto' }}
+                />
             ),
         },
         {
@@ -75,7 +102,7 @@ export default function ProductTable() {
                 </Box>
             ),
         },
-    ], [showDeleted]);
+    ], []);
 
 
 
