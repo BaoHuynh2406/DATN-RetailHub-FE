e266a39b59b90811f548e265fc57019e81944720
nguyenhoa102 <lynguyenhoa102@gmail.com>
2024-10-21 21:07:01 +0700

    NguyenHoa: test layout settings

---
+++ src/pages/Settings/index.jsx
@@ -0,0 +1,9 @@
+function Setting() {
+    return (
+        <div>
+            <h1 className='text-red-800 text-3xl' >Setting with NguyenHoa =))</h1>
+        </div>
+      );
+}
+
+export default Setting;
